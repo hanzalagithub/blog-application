e9b1738832c3a6972e80fc0006c3be46f964c3b3
Ahmed Hanzala <ahmedhanzala2020@gmail.com>
2025-07-13 16:23:19 +0500

    removed-secrets

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,9 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClMSVluajjG1yXXdapeL_fA0gQmvJdbWY",
-  authDomain: "react-blogs-app-50f67.firebaseapp.com",
-  projectId: "react-blogs-app-50f67",
-  storageBucket: "react-blogs-app-50f67.firebasestorage.app",
-  messagingSenderId: "474631191748",
-  appId: "1:474631191748:web:650fc1655a229829c09d91"
+  // firebase
 };
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
